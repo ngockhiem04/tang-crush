65f57b655d812a81d0c681c2d9824e8211217f30
ngockhiem04 <87083719+ngockhiem04@users.noreply.github.com>
2021-07-07 23:40:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Tại vì cậu đẹpppp traiiiii <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Hôm nào rảnh thì đi chơi cùng anh nhé😉.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
